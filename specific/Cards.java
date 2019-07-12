6993b7461f2050cbc7c391abb13143aee0988cc8
AsapChun <schun@bu.edu>
2019-07-12 11:43:52 -0400

    card adds

--- specific/Cards.java
+++ specific/Cards.java
@@ -1,5 +1,21 @@
 package specific;
 
 public class Cards {
-	String number = "one";
+  private int total_cards = 52;
+  private int available_cards[] =  new int[52];
+
+
+  public Cards ( ){ //initializer of cards
+    // (A) 1 , 2, 3, 4, 5, 6, 7, 8, 9, 10, (J) 11, (Q) 12, (K) 13,
+    for(int i = 1; i <= 13; i ++){
+      for(int j = 0; j < 4; j++){
+          available_cards[i]= i;
+      }
+    }
+  }
+
+  
+
+
+
 }
