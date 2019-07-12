f0275238500574fbda31ce94f0cb0d218f1c895e
Ethan <ethango1997@gmail.com>
2019-07-12 12:30:42 -0400

    modified more stuff

--- specific/BlackJack.java
+++ specific/BlackJack.java
@@ -1,28 +1,61 @@
 package specific;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class BlackJack {
 	//attributes
 	Deck default_deck;
 	Dealer dealer;
 	ArrayList<Players> num_player;
+	int round;
 	
 	
 	public BlackJack() {
 		this.default_deck = new Deck();
+		this.dealer = new Dealer();
+		this.num_player = new ArrayList<Players>();
+		this.round = 0;
+	}
+	
+	public void playGame() {
+		startGame();
+		//after initializing players, we keep playing until all the players are out...
+		while(this.num_player.size() > 1) { //until we have 1 winner
+			
+		}
 	}
 	
 	
+	//Copied from the TicTacToe game :)
+	private Players createPlayers() {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("enter your name: ");
+		String name = scan.nextLine();	
+		Players player = new Players(name);
+		return player;
+	}
 	
+	private void startGame() {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("How many players are there: ");
+		int num_players = scan.nextInt();
+		for(int i = 0; i < num_players; i++) {
+			//Create a new players for each input
+			this.num_player.add(createPlayers());
+		}
+	}
 	
+	private void reset() { //a function that resets everything
+		this.default_deck = new Deck();
+		this.num_player = new ArrayList<Players>();
+		this.round = 0;
+		playGame();
+	}
 	
+	//Logic components
 	
-	
-	
-	
-	
-	//methods
+	//main method
 	
 	
 	public static void main(String[] args) {
@@ -30,5 +63,7 @@ public class BlackJack {
 		ArrayList<String> a = new ArrayList<String>();
 		a.add("hi");
 		a.add("bye");
+		System.out.println(a.toString());
+		
 	}
 }
--- specific/Players.java
+++ specific/Players.java
@@ -8,6 +8,13 @@ public class Players {
 	String name;
 	Balance balance;
 	ArrayList<Cards> hand;
+	
+	//This is the in-game attributes of the player
+	boolean still_playing; //whether the player did not fold
+	int amount_on_bet;
+	int score; //this is the score based on the hand that the players has
+	
+	
 
   public Players(String name){
     this.name = name;
@@ -15,8 +22,26 @@ public class Players {
     this.hand = new ArrayList<Cards>();
   }
 
-  //Player should be able to: Hit, Stand, Split, Double up
+  //Player should be able to: Hit, Stand, Split, Double up, Bet
+  public void Bet(int bet_amount) {
+	  this.balance.decreaseBalance(bet_amount);
+	  this.amount_on_bet += bet_amount;
+  }
   
+  public void Hit() {
+	  
+  }
+  
+  public void Stand() {
+	  //This means you're out of the round
+	  this.still_playing = false;
+	  this.amount_on_bet = 0; //you lose that bet
+  }
+  
+  public void DoubleUp() {
+	  
+	  this.amount_on_bet = this.amount_on_bet * 2;
+  }
   
 
 
