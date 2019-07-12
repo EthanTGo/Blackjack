748304e35ddaa57d10e7a9fc1414a1c32ebdd295
Ethan <ethango1997@gmail.com>
2019-07-12 11:17:33 -0400

    a

---
+++ specific/Balance.java
@@ -0,0 +1,23 @@
+package specific;
+
+public class Balance {
+  private int money;
+
+
+
+  //Balance constructor/initializer
+  public Balance(int num){
+    this.money = num;
+  }
+
+  
+
+  //getter
+  public int getMoney(){
+    return this.money;
+  }
+
+
+
+
+}
--- specific/Players.java
+++ specific/Players.java
@@ -3,10 +3,11 @@ package specific;
 public class Players {
   //Should have: name, Balance
 	String name;
-  Balance balance;
+	Balance balance;
 
   public Players(String name){
     this.name = name;
+    this.balance = new Balance(0);
   }
 
   //Player should be able to: Hit, Stand, Split, Double up
