8d988603f78824203c2a7e7e00e9219932b97dec
Ethan <ethango1997@gmail.com>
2019-07-12 11:07:55 -0400

    Added more classes

---
+++ specific/Players.java
@@ -0,0 +1,17 @@
+package specific;
+
+public class Players {
+  //Should have: name, Balance
+	String name;
+  Balance balance;
+
+  public Players(String name){
+    this.name = name;
+  }
+
+  //Player should be able to: Hit, Stand, Split, Double up
+
+
+
+
+}
